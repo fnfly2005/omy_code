35335bfb9b688f98f236051445bde88daaea1a91
fnfly2005 <fnfly2005@aliyun.com>
2018-04-17 20:05:42 +0800

    20180417

--- doc/cw01.sql
+++ doc/cw01.sql
@@ -15,7 +15,7 @@ select
     sum(spo.expense) as expense,
     sum(spo.totalticketprice) as totalticketprice
 from (
-    select partition_date as dt, order_id, sellchannel, customer_id, performance_id, meituan_userid, show_id, totalprice, grossprofit, setnumber, salesplan_count, expressfee, discountamount, income, expense, totalticketprice, project_id, bill_id, salesplan_id from mart_movie.detail_myshow_salepayorder where partition_date>='$$begindate' and partition_date<'$$enddate'
+    select partition_date as dt, order_id, sellchannel, customer_id, performance_id, meituan_userid, show_id, totalprice, grossprofit, setnumber, salesplan_count, expressfee, discountamount, income, expense, totalticketprice, ticket_price, sell_price, project_id, bill_id, salesplan_id, city_id, pay_time from mart_movie.detail_myshow_salepayorder where partition_date>='$$begindate' and partition_date<'$$enddate'
     ) spo
    join (
    select performance_id, activity_id, performance_name, category_id, category_name, area_1_level_name, area_2_level_name, province_name, city_id, city_name, shop_name from mart_movie.dim_myshow_performance where performance_id is not null
--- doc/xk05.sql
+++ doc/xk05.sql
@@ -1,5 +1,6 @@
 
 select
+    ss.dt,
     area_1_level_name,
     province_name,
     city_name,
@@ -30,7 +31,6 @@ from (
             mart_movie.detail_myshow_salepayorder
         where
             partition_date>='$$today{-7d}'
-            and partition_date<'$$today{-0d}'
         group by
             1
         ) spo
@@ -39,57 +39,48 @@ from (
             ) per
         on per.performance_id=spo.performance_id
     ) so
-    left join (
+    join (
         select
-            performance_id,
-            max(bd_name) as bd_name,
-            avg(all_ticketnum) all_ticketnum,
-            avg(us_ticketnum) us_ticketnum,
+            dt,
+            dms.performance_id,
+            max(case when dpr.project_id is null then 0 else bd_name end) bd_name,
+            count(distinct ticketclass_id) as all_ticketnum,
+            count(distinct case when salesplan_sellout_flag=0 and customer_type_id=2 then ticketclass_id end) us_ticketnum
         from (
             select
-                dt,
-                dms.performance_id,
-                max(case when dpr.project_id is null then 0 else bd_name end) bd_name,
-                count(distinct ticketclass_id) as all_ticketnum,
-                count(distinct case when salesplan_sellout_flag=0 and customer_type_id=1 then ticketclass_id end) us_ticketnum,
-            from (
+                partition_date as dt,
+                customer_type_id,
+                performance_id,
+                ticketclass_id,
+                show_id,
+                salesplan_sellout_flag,
+                project_id,
+                city_id
+            from
+                mart_movie.detail_myshow_salesplan
+            where
+                partition_date='$$today{-1d}'
+            ) dms
+            left join (
+                select show_id, performance_id, substr(show_starttime,1,10) as show_starttime, show_endtime from mart_movie.dim_myshow_show where show_id is not null
+                ) sho
+            on sho.show_id=dms.show_id
+            left join (
                 select
-                    partition_date as dt,
-                    customer_type_id,
-                    performance_id,
-                    ticketclass_id,
-                    show_id,
-                    salesplan_sellout_flag,
                     project_id,
-                    city_id
+                    bd_name
                 from
-                    mart_movie.detail_myshow_salesplan
+                    mart_movie.dim_myshow_project
                 where
-                    partition_date>='$$today{-7d}'
-                    and partition_date<'$$today{-0d}'
-                ) dms
-                left join (
-                    select show_id, performance_id, substr(show_starttime,1,10) as show_starttime, show_endtime from mart_movie.dim_myshow_show where show_id is not null
-                    ) sho
-                on sho.show_id=dms.show_id
-                left join (
-                    select
-                        project_id,
-                        bd_name
-                    from
-                        mart_movie.dim_myshow_project
-                    where
-                        bd_name is not null
-                    ) dpr
-                on dpr.project_id=dms.project_id
-            group by
-                1,2
-            ) sp
+                    bd_name is not null
+                ) dpr
+            on dpr.project_id=dms.project_id
         group by
-            1
+            1,2
         ) ss
     on so.performance_id=ss.performance_id
     and so.rank<=50
-where
-    so.rank<=50
+order by
+    area_1_level_name,
+    rank
 ;
