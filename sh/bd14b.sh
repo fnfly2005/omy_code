cf5507e140ecb7d9a49d3fe78e01b6314de3f137
fnfly2005 <fnfly2005@aliyun.com>
2018-05-25 21:55:47 +0800

    20180525

--- sh/bd14b.sh
+++ sh/bd14b.sh
@@ -1,10 +1,31 @@
+#!/bin/bash
+path="/Users/fannian/Documents/my_code/"
+fun() {
+    if [ $2x == dx ];then
+        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed '/where/,$'d`
+    elif [ $2x == ux ];then
+        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed '1,/from/'d | sed '1s/^/from/'`
+    elif [ $2x == tx ];then
+        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed "s/begindate/today{-1d}/g;s/enddate/today{-0d}/g"`
+    elif [ $2x == utx ];then
+        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed "s/begindate/today{-1d}/g;s/enddate/today{-0d}/g" | sed '1,/from/'d | sed '1s/^/from/'`
+    else
+        echo `cat ${path}sql/${1} | grep -iv "/\*"`
+    fi
+}
+spe=`fun myshow_send_performance.sql`
 
+file="bd14"
+lim=";"
+attach="${path}doc/${file}.sql"
+
+echo "
 select 
     mobile,
-    $send_performance_id as send_performance_id,
-    '$$enddate' as send_date,
-    cast(floor(rand()*$batch_code) as bigint)+1 as batch_code,
-    '$sendtag' as sendtag
+    \$send_performance_id as send_performance_id,
+    '\$\$enddate' as send_date,
+    cast(floor(rand()*\$batch_code) as bigint)+1 as batch_code,
+    '\$sendtag' as sendtag
 from (
     select 
         mobile,
@@ -20,8 +41,8 @@ from (
             from
                 mart_movie.detail_myshow_saleorder
             where
-                sellchannel in ($sellchannel_id)
-                and 1 in ($order_src)
+                sellchannel in (\$sellchannel_id)
+                and 1 in (\$order_src)
                 and performance_id in (
                     select distinct
                         performance_id
@@ -31,21 +52,21 @@ from (
                         from
                             mart_movie.dim_myshow_performance
                         where 
-                            category_id in ('$category_id')
+                            category_id in ('\$category_id')
                             and (
-                                performance_id in ($performance_id)
-                                or -99=$performance_id
+                                performance_id in (\$performance_id)
+                                or -99=\$performance_id
                                 )
                             and (
-                                performance_name like '%$performance_name%'
-                                or '测试'='$performance_name'
+                                performance_name like '%\$performance_name%'
+                                or '测试'='\$performance_name'
                                 )
                             and (
-                                shop_name like '%$shop_name%'
-                                or '测试'='$shop_name'
+                                shop_name like '%\$shop_name%'
+                                or '测试'='\$shop_name'
                                 )
                         ) c1
-                    where performance_id not in ($no_performance_id)
+                    where performance_id not in (\$no_performance_id)
                     )
                 and city_id in (
                     select
@@ -53,10 +74,10 @@ from (
                     from
                         mart_movie.dim_myshow_city
                     where
-                        province_id in ($province_id)
+                        province_id in (\$province_id)
                     union
                     select
-                        $city_id as city_id
+                        \$city_id as city_id
                     )
             union all
             select
@@ -70,8 +91,8 @@ from (
                 from
                     mart_movie.dim_wg_userlabel
                 where
-                    2 in ($order_src)
-                    and city_id in ($city_id)
+                    2 in (\$order_src)
+                    and city_id in (\$city_id)
                 union all
                 select 
                     mobile,
@@ -80,12 +101,12 @@ from (
                 from
                     mart_movie.dim_wp_userlabel
                 where
-                    3 in ($order_src)
-                    and city_id in ($city_id)
+                    3 in (\$order_src)
+                    and city_id in (\$city_id)
                 ) ws
                 cross join unnest(category_flag) as t (category_id)
             where
-                category_id in ($category_id)
+                category_id in (\$category_id)
             ) so
             left join (
                 select distinct
@@ -96,26 +117,26 @@ from (
                     from 
                         mart_movie.detail_myshow_msuser
                     where (
-                        (send_date>=date_add('day',-$id,date_parse('$$enddate','%Y-%m-%d'))
-                        and $id<>0)
-                        or sendtag in ('$send_tag')
+                        (send_date>=date_add('day',-\$id,date_parse('\$\$enddate','%Y-%m-%d'))
+                        and \$id<>0)
+                        or sendtag in ('\$send_tag')
                             )
                         and sendtag not in (
-                            select sendtag from upload_table.myshow_send_performance_fn where send_flag='0'
+                            $spe
                             )
                     union all
                     select mobile
                     from upload_table.send_fn_user
                     where (
                         send_date>=current_date
-                        and $id<>0
+                        and \$id<>0
                             )
                     union all 
                     select mobile
                     from upload_table.send_wdh_user
                     where (
                         send_date>=current_date
-                        and $id<>0
+                        and \$id<>0
                             )
                     ) m1
                 ) mm
@@ -127,5 +148,14 @@ from (
         ) as cs
     ) as c
 where
-    rank<=$limit
-;
+    rank<=\$limit
+$lim">${attach}
+
+echo "succuess!"
+echo ${attach}
+if [ ${1}r == pr ]
+#加上任意字符，如r 避免空值报错
+then
+cat ${attach}
+#命令行参数为p时，打印输出文件
+fi
--- sql/dim_myshow_city.sql
+++ sql/dim_myshow_city.sql
@@ -2,13 +2,11 @@
 select 
     city_id,
     mt_city_id,
-    case when mt_city_id=0 then '其他城市'
-    else city_name end as city_name,
-    case when mt_city_id=0 then '其他城市'
-    else province_name end as province_name,
+    city_name,
+    province_name,
     area_1_level_name,
     area_2_level_name
 from
     mart_movie.dim_myshow_city
 where
-    city_id is not null
+    dp_flag=0
