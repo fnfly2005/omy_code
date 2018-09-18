f6a0ffecbdb0f1ba29924a371828f4539485eaab
fnfly2005 <fnfly2005@aliyun.com>
2018-09-18 21:32:41 +0800

    页面埋点配置升级至拉链表、新增名称字段

--- etl/dim_myshow_mv.sql
+++ etl/dim_myshow_mv.sql
@@ -1,16 +1,11 @@
-##-- 这个是sqlweaver(美团自主研发的ETL工具)的编辑模板
-##-- 本模板内容均以 ##-- 开始,完成编辑后请删除
-##-- ##xxxx## 型的是ETL专属文档节点标志, 每个节点标志到下一个节点标志为本节点内容
-##-- 流程应该命名成: 目标库dsn名.目标表名
-
 ##Description##
-##-- 这个节点填写本ETL的描述信息, 包括目标表定义, 建立时的需求jira编号等
+
 
 ##TaskInfo##
 creator = 'fannian@meituan.com'
 
 source = {
-    'db': META['hmart_movie'], ##-- 单引号内填写一个dsn库名，表示Extract阶段的SQL在哪个数据库里执行
+    'db': META['hmart_movie'], 
 }
 
 stream = {
@@ -18,21 +13,20 @@ stream = {
 }
 
 target = {
-    'db': META['hmart_movie'], ##-- 单引号内填写目标库的dsn名
-    'table': 'dim_myshow_mv', ##-- 单引号中填写目标表名
+    'db': META['hmart_movie'], 
+    'table': 'dim_myshow_mv', 
 }
 
 ##Extract##
-##-- Extract节点, 这里填写一个能在source.db上执行的、读取数据的sql
 
 ##Preload##
-##-- Preload节点, 这里填写一个能在target.db上执行的、load数据之前执行的sql(可以留空)
+
 #if $isRELOAD
 drop table `$target.table`
 #end if
 
 ##Load##
-##-- Load节点, 这里填写一个能在target.db上执行的、load数据的sql(可以留空)
+
 insert OVERWRITE TABLE `$target.table`
 select
     mv_id,
@@ -49,12 +43,12 @@ select
     end_date,
     page_name_my,
     cid_type,
-    operation_flag
+    operation_flag,
+    user_intention
 from 
-    upload_table.my_mv_fn_s
+    upload_table.my_mv_fn_v3
 
 ##TargetDDL##
-##-- 目标表表结构
 CREATE TABLE IF NOT EXISTS `$target.table`
 (
 `mv_id` int COMMENT '配置ID',
@@ -62,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `$target.table`
 `event_name_lv1` string COMMENT '一级模块名',
 `event_name_lv2` string COMMENT '二级模块名',
 `page_identifier` string COMMENT '所属页面',
-`user_int` int COMMENT '用户意向',
+`user_int` int COMMENT '用户意向-id',
 `biz_par` string COMMENT '业务参数',
 `biz_typ` string COMMENT '逻辑字段',
 `page_loc` int COMMENT '页面位置',
@@ -71,7 +65,8 @@ CREATE TABLE IF NOT EXISTS `$target.table`
 `end_date` string COMMENT '闭链日期',
 `page_name_my` string COMMENT '页面名称',
 `cid_type` string COMMENT '埋点逻辑类型',
-`operation_flag` int COMMENT '是否运营位 0 否 1 是'
+`operation_flag` int COMMENT '是否运营位 0 否 1 是',
+`user_intention` string COMMENT '用户意向-名称'
 ) COMMENT '演出模块埋点配置拉链表'
 ROW FORMAT DELIMITED 
 FIELDS TERMINATED BY '\t'
--- etl/dim_myshow_pv.sql
+++ etl/dim_myshow_pv.sql
@@ -1,8 +1,32 @@
-creator='fannian@meituan.com'
-'db': META['hmart_movie'],
-'table': 'dim_myshow_pv'
+##Description##
 
-'format': 'pv_id,page_identifier,page_name_my,cid_type,page_cat,biz_par,biz_bg,status,modify_date',
+
+
+##TaskInfo##
+creator = 'fannian@meituan.com'
+
+source = {
+    'db': META['hmart_movie'], 
+}
+
+stream = {
+    'format': '',
+}
+
+target = {
+    'db': META['hmart_movie'], 
+    'table': 'dim_myshow_pv', 
+}
+
+##Extract##
+
+
+##Preload##
+#if $isRELOAD
+drop table `$target.table`
+#end if
+
+##Load##
 
 insert OVERWRITE TABLE `$target.table`
 select
@@ -14,28 +38,29 @@ select
     biz_par,
     biz_bg,
     status,
-    modify_date
+    begin_date,
+    end_date,
+    page_intention,
+    biz_bg_name
 from 
-    upload_table.my_pv_fn
-
-#if $isRELOAD
-drop table `$target.table`
-#end if
+    upload_table.my_pv_fn_v2
 
 ##TargetDDL##
-##-- 目标表表结构
 CREATE TABLE IF NOT EXISTS `$target.table`
 (
 `pv_id` int COMMENT '配置ID',
 `page_identifier` string COMMENT '标识',
 `page_name_my` string COMMENT '名称',
 `cid_type` string COMMENT '埋点类型',
-`page_cat` int COMMENT '页面分类',
+`page_cat` int COMMENT '页面意向_id',
 `biz_par` string COMMENT '业务参数',
-`biz_bg` int COMMENT '业务归属',
+`biz_bg` int COMMENT '业务归属_id',
 `status` int COMMENT '是否最新',
-`modify_date` string COMMENT '维护日期'
-) COMMENT '演出页面埋点配置表'
+`begin_date` string COMMENT '开链日期',
+`end_date` string COMMENT '闭链日期',
+`page_intention` string COMMENT '页面意向-名称',
+`biz_bg_name` string COMMENT '业务归属-名称'
+) COMMENT '演出页面埋点配置拉链表'
 ROW FORMAT DELIMITED 
 FIELDS TERMINATED BY '\t'
 stored as orc
