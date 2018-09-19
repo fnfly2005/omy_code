35dbe5bf313a9e046b68cb1532c54a8bd214335d
fnfly2005 <fnfly2005@aliyun.com>
2018-09-19 16:17:52 +0800

    上游变动，更新逻辑

--- sh/bs16d.sh
+++ sh/bs16d.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-#--------------------猫眼演出readme-------------------
-#*************************api1.0*******************
-# 优化输出方式,优化函数处理
-path=""
-fun() {
-    if [ $2x == dx ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed '/where/,$'d`
-    elif [ $2x == ux ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed '1,/from/'d | sed '1s/^/from/'`
-    elif [ $2x == tx ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed "s/begindate/today{-1d}/g;s/enddate/today{-0d}/g"`
-    elif [ $2x == utx ];then
-        echo `cat ${path}sql/${1} | grep -iv "/\*" | sed "s/begindate/today{-1d}/g;s/enddate/today{-0d}/g" | sed '1,/from/'d | sed '1s/^/from/'`
-    else
-        echo `cat ${path}sql/${1} | grep -iv "/\*"`
-    fi
-}
-
-md=`fun myshow_dictionary.sql`
+#页面模块流量-分日期/平台
+source ./fuc.sh
+md=`fun dim_myshow_dictionary.sql`
 file="bs16"
 lim=";"
 attach="${path}doc/${file}.sql"
@@ -70,8 +54,6 @@ from (
     join mart_movie.dim_myshow_mv mp
     on mp.event_id=fmw.event_id
     and mp.page_identifier=fmw.page_identifier
-    left join mart_movie.dim_myshow_pv msp
-    on mp.page_identifier=msp.page_identifier
 where
     md.value2 in ('\$app_name')
 group by 
