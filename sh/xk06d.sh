f6150d1f19b01ad43000747047efa220dd413bee
fnfly2005 <fnfly2005@aliyun.com>
2018-09-21 16:02:19 +0800

    新增埋点业务参数监控指标

--- sh/xk06d.sh
+++ sh/xk06d.sh
@@ -20,19 +20,24 @@ select
     page_cat,
     biz_bg,
     biz_par,
-    uv
+    sum(uv) as uv,
+    sum(custom_uv) as custom_uv
 from (
     $mp
     ) mp
     left join (
         select 
             page_identifier,
-            approx_distinct(union_id) uv
+            app_name,
+            approx_distinct(union_id) uv,
+            approx_distinct(case when page_name_my='演出详情页' and performance_id is not null then union_id end) custom_uv
         $ort
         group by
-            1
+            1,2
         ) as fpw
     on mp.page_identifier=fpw.page_identifier
+group by
+    1,2,3,4,5,6,7,8,9
 $lim">${attach}
 
 echo "succuess!"
