cd677f83966fdab32c4b2850e217a87f3f1e212c
fnfly2005 <fnfly2005@aliyun.com>
2018-09-25 20:51:46 +0800

    支持开售提醒数据源、导入美团用户ID数据源

--- sh/bd17.sh
+++ sh/bd17.sh
@@ -1,23 +1,53 @@
 #!/bin/bash
-path="/Users/fannian/Documents/my_code/"
-t1='$time1'
-fun() {
-echo `cat ${path}sql/${1} | sed "s/'-time3'/substr(date_add('day',-1,timestamp'$t1'),1,10)/g" | grep -iv "/\*"`
-}
+#非营销手机号提取
+source ./fuc.sh
 
-mp=`fun dp_myshow__s_messagepush.sql` 
-file="bd17"
+mp=`fun dp_myshow__s_messagepush.sql uD`
+ser=`fun dim_myshow_movieuser.sql ud`
+era=`fun dim_myshow_movieusera.sql ud`
+rid=`fun sql/myshowupload_user_id.sql`
+file=`echo $0 | sed "s/[a-z]*\.sh//g;s/.*\///g"`
 lim=";"
 attach="${path}doc/${file}.sql"
 
 echo "
-select
+select distinct 
     mobile
-from (
+from ( 
+    select
+        phonenumber as mobile    
     $mp
-    and performanceid=\$id
-    ) mp
+        performanceid in (\$id)
+        and 5 in (\$urc)
+    union all
+    select
+        mobile 
+    from (
+        $rid
+            and 8 in (\$urc)
+        ) rid
+        left join (
+            select
+                user_id,
+                mobile
+            $ser
+            union all
+            select
+                user_id,
+                mobile
+            $era
+            ) sra
+        on rid.user_id=sra.user_id
+    where
+        sra.mobile is not null
+    ) as ile
 $lim">${attach}
 
-echo "succuess,detail see ${attach}"
-
+echo "succuess!"
+echo ${attach}
+if [ ${1}r == pr ]
+#加上任意字符，如r 避免空值报错
+then
+cat ${attach}
+#命令行参数为p时，打印输出文件
+fi
